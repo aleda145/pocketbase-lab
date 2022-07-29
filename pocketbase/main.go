d7f558eb25bf769055abd785065b421372994fdd
Alexander Dahl <alex@dahl.dev>
2022-07-29 13:45:44 +0200

    init

---
+++ pocketbase/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/labstack/echo/v5"
+	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/apis"
+	"github.com/pocketbase/pocketbase/core"
+)
+
+func main() {
+	fmt.Println("Initalizating pocketbase")
+	app := pocketbase.New()
+
+	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
+		// serves static files from the provided public dir (if exists)
+		subFs := echo.MustSubFS(e.Router.Filesystem, "pb_public")
+		e.Router.GET("/*", apis.StaticDirectoryHandler(subFs, false))
+
+		return nil
+	})
+	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
+		e.Router.AddRoute(echo.Route{
+			Method: http.MethodGet,
+			Path:   "/api/hello",
+			Handler: func(c echo.Context) error {
+				return c.String(200, "Hello world!")
+			},
+			// Middlewares: []echo.MiddlewareFunc{
+			// 	apis.RequireAdminOrUserAuth(),
+			// },
+		})
+
+		return nil
+	})
+
+	if err := app.Start(); err != nil {
+		log.Fatal(err)
+	}
+}
