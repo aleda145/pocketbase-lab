f6ce779979ac953ac5ff06a2fb8df70369626ae8
Alexander Dahl <alex@dahl.dev>
2022-08-02 18:10:37 +0200

    Dont refetch on window focus

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,13 @@ import Preferences from "./Preferences/Preferences";
 import PocketBase from "pocketbase";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 function App() {
   const [token, setToken] = useState();
   console.log(token);
