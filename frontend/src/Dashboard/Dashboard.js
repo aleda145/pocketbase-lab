828421a88ab7fe928d33a744c4e934dc65cd11d1
Alexander Dahl <alex@dahl.dev>
2022-08-02 17:43:42 +0200

    use useeffect

--- frontend/src/Dashboard/Dashboard.js
+++ frontend/src/Dashboard/Dashboard.js
@@ -1,6 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 export default function Dashboard({ client }) {
-  console.log(client);
-  return <h2>Dashboard {client.AuthStore.model.email}</h2>;
+  const getCars = async () => {
+    client.Records.getList("user_nums", 1, 50, {
+      filter: "created >= '2022-01-01 00:00:00'",
+    })
+      .then(function (list) {
+        // success...
+        console.log("list", list);
+      })
+      .catch(function (error) {
+        // error...
+        console.error("getlist error", error);
+      });
+  };
+  //   console.log(client);
+  useEffect(() => {
+    getCars();
+  });
+  return (
+    <div>
+      <h2>Dashboard {client.AuthStore.model.email}</h2>
+      {/* <div>{getCars()}</div> */}
+    </div>
+  );
 }
