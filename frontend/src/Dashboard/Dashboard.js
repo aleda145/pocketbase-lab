9655f832e2e3575bf328ac0de79d2a78308cdb70
Alexander Dahl <alex@dahl.dev>
2022-08-02 22:26:48 +0200

    Add add number

--- frontend/src/Dashboard/Dashboard.js
+++ frontend/src/Dashboard/Dashboard.js
@@ -1,16 +1,37 @@
 import React, { useEffect } from "react";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { useNavigate, NavLink } from "react-router-dom";
 
 export default function Dashboard({ client, setIsLoggedIn }) {
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
 
+  const addMutation = useMutation(
+    (url) => {
+      // return axios.post(getApiDomain() + "/scrape", url);
+      // return "ok";
+      return client.Records.create("user_nums", {
+        user: "KoDn7hCUaY8BIGG",
+        num: 42,
+      });
+    },
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries("favNumbers");
+        console.log("succes");
+      },
+      onError: (error) => {
+        console.log(error);
+        error.customError = error.response.data.message;
+      },
+    }
+  );
   async function logOutClicked() {
     await client.AuthStore.clear();
     setIsLoggedIn(false);
     navigate("/");
   }
-  const { isLoading, error, data } = useQuery(["repoData"], () =>
+  const { isLoading, error, data } = useQuery(["favNumbers"], () =>
     client.Records.getList("user_nums", 1, 50, {
       filter: "created >= '2022-01-01 00:00:00'",
     })
@@ -23,10 +44,27 @@ export default function Dashboard({ client, setIsLoggedIn }) {
   for (const item of data.items) {
     favorite_numbers.push(item.num + " ");
   }
+
   return (
     <div>
       <div>My {client.AuthStore.model.email} favorite numbers</div>
       <div>{favorite_numbers}</div>
+
+      <button
+        onClick={() => {
+          addMutation.mutate(
+            { url: "ok" },
+            {
+              onSuccess: () => {
+                console.log("button scucces");
+              },
+            }
+          );
+        }}
+      >
+        Lägg till nummer
+      </button>
+
       <button onClick={logOutClicked}>Log out</button>
     </div>
   );
