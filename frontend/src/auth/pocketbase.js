40fbbf428e72c36016a9b5982312785fbab81213
Alexander Dahl <alex@dahl.dev>
2022-08-03 10:19:56 +0200

    Add signup

--- frontend/src/auth/pocketbase.js
+++ frontend/src/auth/pocketbase.js
@@ -10,10 +10,38 @@ export default function Auth({ client, setIsLoggedIn }) {
     console.log(userData);
     setIsLoggedIn(client.AuthStore.isValid);
   };
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+    const user = await client.Users.create({
+      email: username,
+      password: password,
+      passwordConfirm: password,
+    });
+
+    console.log(user);
+    // setIsLoggedIn(client.AuthStore.isValid);
+  };
   return (
     <div className="login-wrapper">
       <h1>Please Log In</h1>
       <form onSubmit={handleSubmit}>
+        <label>
+          <p>Username</p>
+          <input type="text" onChange={(e) => setUserName(e.target.value)} />
+        </label>
+        <label>
+          <p>Password</p>
+          <input
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+      <h2>Or sign up</h2>
+      <form onSubmit={handleSignUp}>
         <label>
           <p>Username</p>
           <input type="text" onChange={(e) => setUserName(e.target.value)} />
