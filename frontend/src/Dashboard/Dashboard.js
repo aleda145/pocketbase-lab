35104d279300d8dfecb231f81d9af9448f0c921d
Alexander Dahl <alex@dahl.dev>
2022-08-03 14:54:37 +0200

    Add delete number

--- frontend/src/Dashboard/Dashboard.js
+++ frontend/src/Dashboard/Dashboard.js
@@ -24,6 +24,23 @@ export default function Dashboard({ client, setIsLoggedIn }) {
       },
     }
   );
+  const removeMutation = useMutation(
+    (numId) => {
+      console.log(numId.numId);
+      console.log(typeof numId.numId);
+      return client.Records.delete("user_nums", numId.numId);
+    },
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries("favNumbers");
+        console.log("succes");
+      },
+      onError: (error) => {
+        console.log(error);
+        error.customError = error.response.data.message;
+      },
+    }
+  );
   async function logOutClicked() {
     await client.AuthStore.clear();
     setIsLoggedIn(false);
@@ -39,13 +56,16 @@ export default function Dashboard({ client, setIsLoggedIn }) {
 
   if (error) return "An error has occurred: " + error.message;
   let favorite_numbers = [];
+  let favorite_ids = [];
   for (const item of data.items) {
     favorite_numbers.push(item.num + " ");
+    favorite_ids.push(item.id);
   }
 
   return (
     <div>
       <div>My {client.AuthStore.model.email} favorite numbers</div>
+      <div>{favorite_ids}</div>
       <div>{favorite_numbers}</div>
 
       <button
@@ -61,6 +81,20 @@ export default function Dashboard({ client, setIsLoggedIn }) {
         }}
       >
         Lägg till nummer
+      </button>
+      <button
+        onClick={() => {
+          removeMutation.mutate(
+            { numId: favorite_ids[0] },
+            {
+              onSuccess: () => {
+                console.log("button remove scucces");
+              },
+            }
+          );
+        }}
+      >
+        Delete number
       </button>
 
       <button onClick={logOutClicked}>Log out</button>
