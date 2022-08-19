22eb6e46feae9a19dcf32c1ff888cba288aa87f9
Alexander Dahl <alex@dahl.dev>
2022-08-19 20:13:47 +0200

    Remove unused file serving

--- backend/pocketbase/main.go
+++ backend/pocketbase/main.go
@@ -9,7 +9,6 @@ import (
 
 	"github.com/labstack/echo/v5"
 	"github.com/pocketbase/pocketbase"
-	"github.com/pocketbase/pocketbase/apis"
 	"github.com/pocketbase/pocketbase/core"
 )
 
@@ -18,22 +17,12 @@ func main() {
 	app := pocketbase.New()
 
 	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
-		// serves static files from the provided public dir (if exists)
-		subFs := echo.MustSubFS(e.Router.Filesystem, "pb_public")
-		e.Router.GET("/*", apis.StaticDirectoryHandler(subFs, false))
-
-		return nil
-	})
-	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
 		e.Router.AddRoute(echo.Route{
 			Method: http.MethodGet,
 			Path:   "/api/hello",
 			Handler: func(c echo.Context) error {
 				return c.String(200, "Hello world!")
 			},
-			// Middlewares: []echo.MiddlewareFunc{
-			// 	apis.RequireAdminOrUserAuth(),
-			// },
 		})
 
 		return nil
