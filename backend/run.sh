da5a2869f27ab608778e6337ab74d8f03d67369d
Alexander Dahl <alex@dahl.dev>
2022-08-19 19:06:02 +0200

    Remove comment

--- backend/run.sh
+++ backend/run.sh
@@ -7,16 +7,6 @@ df -h
 echo "litestream version"
 litestream version
 
-# # Docker entry point for image will decode the base64 secret and put it into the GOOGLE_APPLICATION_CREDENTIALS PATH that has been specified
-# # this has to be done before litestream starts!
-# # Should be no issue with the correct entrypoint
-# # then it can be used by flyctl secrets as well
-# # https://www.avaitla16.com/google-application-credentials-json-as-an-environment-variable
-# # https://community.fly.io/t/how-are-you-managing-cert-files-with-fly/2984/20
-
-# echo "set gcp key"
-# echo "$GCP_KEY" | base64 -d > "$GOOGLE_APPLICATION_CREDENTIALS"
-
 echo "Restore db if exists"
 litestream restore -if-replica-exists /pb_data/data.db
 litestream restore -if-replica-exists /pb_data/logs.db
